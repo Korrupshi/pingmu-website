a7a068c6384165b2929c9670243eff43d5238607
Korrupshi <stefan_meijs1@hotmail.com>
2024-02-06 19:59:35 +0100

    strings extracted into constants, web analytics added

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const heroTitle = "Turn Chinese videos into learning ";
+export const heroSpanText = "opportunities";
+export const heroDescription =
+  "Enjoy your language learning journey with Pingmu's immersive videos and powerful learning tools";
